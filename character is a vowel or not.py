02b89c5b2ebf500150e8fe68eb3dfdbfeb9254d4
Vardhaman Pandit <146078618+Vardhaman599@users.noreply.github.com>
2023-10-12 12:00:35 +0530

    Add files via upload

---
+++ character is a vowel or not.py
@@ -0,0 +1,6 @@
+# Create a program that checks if a given character is a vowel.
+char = input("Enter a character:").lower()
+if char == 'a' or char=='e' or char =='i' or char =='o' or char=='u':
+    print("vowel")
+else:
+        print("Consonant")
