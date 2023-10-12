83060f72b75ffa6eaab82ca1ff6b5f9ebbe3cc96
Vardhaman Pandit <146078618+Vardhaman599@users.noreply.github.com>
2023-10-12 12:15:58 +0530

    Add files via upload

---
+++ number is positive or negative or zero.py
@@ -0,0 +1,8 @@
+# Write a program that calculates the grade based on a student's score.
+num = float(input("Enter a number:"))
+if num>0:
+    print("Positive")
+elif num<0:
+    print("Negative")
+else:
+    print("zero")
